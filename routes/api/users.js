c1eb97fc9ff6fa277cfeb6c0a2ec8d89270c203d
Kevin Stradtman <stradtkt@gmail.com>
2018-09-21 14:46:22 -0400

    Added the register component with validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,7 +13,7 @@ const validateLoginInput = require('../../validation/login');
 router.post('/register', (req,res) => {
     const {errors, isValid} = validateRegisterInput(req.body);
     if(!isValid) {
-        return res.status(400).json({errors});
+        return res.status(400).json(errors);
     }
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -50,7 +50,7 @@ router.post('/register', (req,res) => {
 router.post('/login', (req,res) => {
     const {errors, isValid} = validateLoginInput(req.body);
     if(!isValid) {
-        return res.status(400).json({errors});
+        return res.status(400).json(errors);
     }
     const email = req.body.email;
     const password = req.body.password;
