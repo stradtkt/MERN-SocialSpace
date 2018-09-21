6e79d89f43ff6ce5b919e29fc873efc13fdc570e
Kevin Stradtman <stradtkt@gmail.com>
2018-09-20 22:40:13 -0400

    Add like and unline routes to posts

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
-
+const Profile = require('../../models/Profile');
 const Post = require('../../models/Post');
 const validatePostInput = require('../../validation/post');
 
@@ -34,6 +34,19 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     newPost.save().then(post => res.json(post));
 });
 
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req,res) => {
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    if(post.user.toString() !== req.user.id) {
+                        return res.status(401).json({notauthorized: 'user not authorized'});
+                    }
+                    post.remove().then(() => res.json({success: true}))
+                })
+                .catch(err => res.status(404).json({nopostfound: 'No post found'}));
+        });
+});
 
 
 module.exports = router;
